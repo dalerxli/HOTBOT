e50ab7a86dd35510995fbb432446c428caab0147
Matthew Griffiths <mg542@talyn.ch.cam.ac.uk>
2014-06-25 18:28:13 +0100

    edit

--- testscript.sh
+++ testscript.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 ## Please refer to your grid documentation for available flags. This is only an example.
 #PBS -q s48
-#PBS -l procs=16
+##PBS -l procs=16
 #PBS -V
 #PBS -N SCOOPJob
 
+
+
+echo "Starting job $PBS_JOBID"
+echo
+echo "PBS assigned me this node:"
+cat $PBS_NODEFILE
+echo
+
+
 # Path to your executable. For example, if you extracted SCOOP to $HOME/downloads/scoop
-cd $HOME/downloads/scoop/examples
+cd $HOME/Source/HOTBOT
 
 # Add any addition to your environment variables like PATH. For example, if your local python installation is in $HOME/python
-export PATH=$HOME/python/bin:$PATH
+#export PATH=$HOME/python/bin:$PATH
 
 # If, instead, you are using the python offered by the system, you can stipulate it's library path via PYTHONPATH
 #export PYTHONPATH=$HOME/wanted/path/lib/python+version/site-packages/:$PYTHONPATH
@@ -17,4 +26,10 @@ export PATH=$HOME/python/bin:$PATH
 #workon yourenvironment
 
 # Launch SCOOP using the hosts
-python -m scoop -vv fullTree.py
+python -m scoop -vv full_tree.py > output
+
+echo
+echo "Job finished. PBS details are:"
+echo
+qstat -f ${PBS_JOBID}
+echo
