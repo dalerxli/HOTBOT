6bf87b96cda12d3519213a9ab347eb68c34807c6
Matthew Griffiths <mg542@talyn.ch.cam.ac.uk>
2014-06-25 18:41:30 +0100

    testscript.sh~

--- testscript.sh
+++ testscript.sh
@@ -12,7 +12,8 @@ echo
 echo "PBS assigned me this node:"
 cat $PBS_NODEFILE
 echo
-
+echo "PBS_O_WORKDIR"
+echo PBS_O_WORKDIR
 
 # Path to your executable. For example, if you extracted SCOOP to $HOME/downloads/scoop
 cd $HOME/Source/HOTBOT
@@ -25,8 +26,17 @@ cd $HOME/Source/HOTBOT
 # Or use VirtualEnv via virtualenvwrapper here:
 #workon yourenvironment
 
+cd $PBS_O_WORKDIR
+TMP=/scratch/cjf41/$PBS_JOBID
+mkdir -p $TMP
+cd $TMP
+python -m scoop -vv $HOME/Source/HOTBOT/full_tree.py > output
+cp * $PBS_O_WORKDIR
+cd $PBS_O_WORKDIR
+rm -rf $TMP
+
 # Launch SCOOP using the hosts
-python -m scoop -vv full_tree.py > output
+python -m scoop -vv $HOME/Source/HOTBOT/full_tree.py > output
 
 echo
 echo "Job finished. PBS details are:"
